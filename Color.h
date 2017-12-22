c65f11d5089261830e496d85259a7bea6c4a544a
Sander Jansen <sanderjansen1997@hotmail.com>
2017-12-22 12:38:39 +0100

    changed the colorvalues in the defines

--- Color.h
+++ Color.h
@@ -4,20 +4,20 @@
 #define GREEN set_color(0, 255, 0)
 #define BLUE set_color(0, 0, 255)
 #define WHITE set_color(255, 255, 255)
-#define ORANGE set_color(255, 165, 0)
+#define ORANGE set_color(255, 90, 0)
 #define DARK_YELLOW set_color(204, 204, 0)
-#define YELLOW set_color(255, 255, 0)
-#define STRAW_YELLOW set_color(204, 230, 0)
+#define YELLOW set_color(255, 130, 0)
+#define STRAW_YELLOW set_color(255, 206, 0)
 
-#define PEA_GREEN set_color(0, 255, 127)
+#define PEA_GREEN set_color(44, 255, 22)
 #define CYAN set_color(0, 255, 255)
-#define LIGHT_BLUE set_color(173, 216, 230)
-#define SKY_BLUE set_color(0, 191, 255)
+#define LIGHT_BLUE set_color(0, 147, 255)
+#define SKY_BLUE set_color(51, 197, 255)
 
 #define DARK_BLUE set_color(0, 0, 139)
-#define DARK_PINK set_color(199, 21, 133)
-#define PINK set_color(255, 20, 147)
-#define PURPLE set_color(128, 0, 128)
+#define DARK_PINK set_color(137, 31, 202)
+#define PINK set_color(137, 0, 142)
+#define PURPLE set_color(255, 0, 142)
 
 class Color {
 private:
